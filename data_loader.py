280ca1a63bfb15e2022d7deb9ee9c57f01356b2b
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-16 16:48:18 -0700

    Data Loader

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,17 @@
 #Init
+import tensorflow as tf
+import os
+
+train_dir_path = "data/"
+filename = train_dir_path + "train.csv"
+
+with tf.Session() as sess:
+    sess.run( tf.global_variables_initializer())
+    with open(filename) as inf:
+        # Skip header
+        next(inf)
+        for line in inf:
+            # Read data, using python, into our features
+            acoustic_data,time_to_failure = line.strip().split(",")
+            # Run the Print ob
+            print(acoustic_data,time_to_failure)
