77e17b0fab8bbba7cab80b4c2a9a2d8de1987333
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-16 18:14:30 -0700

    Pandas CSV chunking

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,31 @@
 #Init
 import tensorflow as tf
+import pandas as pd
+tf.enable_eager_execution()
+
+FILENAME = 'data.tfrecords'
+
+import numpy as np
+
 import os
 
 train_dir_path = "data/"
 filename = train_dir_path + "train.csv"
+chunksize = 10 ** 8
+
+acoustic_data = []
+time_to_failure = []
 
 with tf.Session() as sess:
-    sess.run( tf.global_variables_initializer())
-    with open(filename) as inf:
-        # Skip header
-        next(inf)
-        for line in inf:
-            # Read data, using python, into our features
-            acoustic_data,time_to_failure = line.strip().split(",")
-            # Run the Print ob
-            print(acoustic_data,time_to_failure)
+    sess.run(tf.global_variables_initializer())
+    for chunk in pd.read_csv(filename, chunksize=chunksize):
+        csv = chunk.values
+        with tf.python_io.TFRecordWriter(FILENAME) as writer:
+            for row in csv:
+                features, label = row[:-1], row[-1]
+                example = tf.train.Example()
+                example.features.feature["features"].float_list.value.extend(features)
+                example.features.feature["label"].float_list.value.append(label)
+                writer.write(example.SerializeToString())
+
+#150k lines of input
