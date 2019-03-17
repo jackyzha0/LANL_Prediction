7c1848c138c0c7bd013d9f8efe64fbed898df0ad
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-16 18:49:32 -0700

    Changed to tf CsvDataset

--- data_loader.py
+++ data_loader.py
@@ -1,31 +1,25 @@
-#Init
 import tensorflow as tf
-import pandas as pd
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 
 FILENAME = 'data.tfrecords'
 
 import numpy as np
-
 import os
 
 train_dir_path = "data/"
 filename = train_dir_path + "train.csv"
-chunksize = 10 ** 8
-
-acoustic_data = []
-time_to_failure = []
+filenames = [filename]
+record_defaults = [tf.float32] * len(filenames)  # Only provide defaults for the selected columns
+x_dataset = tf.contrib.data.CsvDataset(filenames, record_defaults, header=True, select_cols=[0])
+y_dataset = tf.contrib.data.CsvDataset(filenames, record_defaults, header=True, select_cols=[1])
+dataset = tf.data.Dataset.zip((x_dataset, y_dataset))
+fix_len = 150000
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for chunk in pd.read_csv(filename, chunksize=chunksize):
-        csv = chunk.values
-        with tf.python_io.TFRecordWriter(FILENAME) as writer:
-            for row in csv:
-                features, label = row[:-1], row[-1]
-                example = tf.train.Example()
-                example.features.feature["features"].float_list.value.extend(features)
-                example.features.feature["label"].float_list.value.append(label)
-                writer.write(example.SerializeToString())
 
-#150k lines of input
+    batched_dataset = dataset.batch(150000)
+    iterator = batched_dataset.make_one_shot_iterator()
+    next_element = iterator.get_next()
+
+    print(sess.run(next_element))
