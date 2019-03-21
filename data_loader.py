088f454f1531ab9f835d7ecbc16c2b28cf51c372
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-20 20:30:45 -0700

    Network architecture

--- data_loader.py
+++ data_loader.py
@@ -6,6 +6,11 @@ import os
 
 learning_rate = 1e-3
 batchsize = 16
+
+#LSTM Params
+num_hidden = 50
+num_classes = 10
+
 train_dir_path = "data/"
 filename = train_dir_path + "train.csv"
 fix_len = 150000
@@ -56,7 +61,49 @@ def plot(x):
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
+    #Define network architecture
     x_acoustic = tf.placeholder(tf.float32, [None, fix_len])
     y_timetofailure = tf.placeholder(tf.float32, [None, fix_len])
 
-    x = get_batch(batchsize)
+    x_acoustic = tf.expand_dims(x_acoustic, -1)
+
+    def RNN(x):
+
+        weights = {'out': tf.Variable(tf.random_normal([num_hidden, num_classes]))}
+        biases = {'out': tf.Variable(tf.random_normal([num_classes]))}
+
+        # Prepare data shape to match `rnn` function requirements
+        # Current data input shape: (batch_size, timesteps, n_input)
+        # Required shape: 'timesteps' tensors list of shape (batch_size, n_input)
+
+        # Unstack to get a list of 'timesteps' tensors of shape (batch_size, n_input)
+        x = tf.unstack(x, 268, 1)
+
+        # Define a lstm cell with tensorflow
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_hidden, forget_bias=1.0)
+
+        # Get lstm cell output
+        outputs, states = tf.nn.static_rnn(lstm_cell, x, dtype=tf.float32)
+
+        # Linear activation, using rnn inner loop last output
+        return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+    net = tf.layers.conv1d(x_acoustic, 16, 10000, strides = 3) #Form [input layer, #filters, kernel_size]
+    print(net.get_shape())
+    net = tf.layers.conv1d(net, 8, 5000, strides = 5)
+    print(net.get_shape())
+    net = tf.layers.conv1d(net, 4, 1000, strides = 2)
+    print(net.get_shape())
+    net = tf.layers.conv1d(net, 2, 500, strides = 5)
+    print(net.get_shape())
+    net = tf.layers.conv1d(net, 1, 100, strides = 2)
+    print(net.get_shape())
+
+    net = RNN(net)
+    print(net.get_shape())
+
+    net = tf.contrib.layers.fully_connected(net, 1)
+    print(net.get_shape())
+
+    # x = get_batch(batchsize)
+    # print(x.shape)
