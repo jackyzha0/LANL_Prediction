c72fcfcb069d6e154dcf55293a290e9de2536d38
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-25 09:39:53 -0700

    Added reduce mean

--- net.py
+++ net.py
@@ -3,6 +3,9 @@ import os
 import tensorflow as tf
 import numpy as np
 from datetime import datetime
+import sys
+
+np.set_printoptions(threshold=sys.maxsize)
 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -15,7 +18,7 @@ config = tf.ConfigProto()
 ### PARAMETERS ###
 batchsize = 4
 fix_len = 150000
-learning_rate = 1e-3
+learning_rate = 1e-4
 epochs = 10
 dbsize = 6000000
 
@@ -37,30 +40,30 @@ with graph.as_default():
     f_net = tf.contrib.layers.flatten(net)
     print(f_net.get_shape())
 
-    logits = tf.contrib.layers.fully_connected(f_net, 1)
+    logits = tf.layers.dense(f_net, 1)
     print('_____')
     print(logits.get_shape())
     print(y_timetofailure.get_shape())
 
-    loss = tf.losses.mean_squared_error(y_timetofailure, logits)
+    # loss = tf.losses.mean_squared_error(logits, y_timetofailure)
+    loss = tf.reduce_mean(tf.losses.mean_squared_error(logits, y_timetofailure))
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
 with tf.Session(graph=graph, config=config) as sess:
     tf.global_variables_initializer().run()
     tf.local_variables_initializer().run()
 
-    for epoch in range(epochs):
-        for batch in range(int(dbsize / (fix_len * batchsize))):
-            batch = csvtools.get_batch(batchsize)
-            x,y = np.split(batch, 2, axis = 1)
-            x = np.squeeze(x, axis=(1,))
-            y = np.squeeze(y, axis=(1,))[:,-1:]
+    # for epoch in range(epochs):
+    #     for batch in range(int(dbsize / (fix_len * batchsize))):
+    for i in range(10):
+        batch = csvtools.get_batch(batchsize)
+        x,y = np.split(batch, 2, axis = 1)
+        x = np.squeeze(x, axis=(1,))
+        y = np.squeeze(y, axis=(1,))[:,-1:]
 
-            print(x.shape, y.shape)
-
-            print('Running for logits...',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            out = sess.run([optimizer, loss, logits], feed_dict={x_acoustic: x, y_timetofailure: y})
-            print('Pred: ' + str(np.array(out)[2]))
-            print('Actual: ' + str(y))
-            print('Loss: ' + str(np.array(out)[1]))
-            print('______')
+        print('Running for logits...',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        out = sess.run([optimizer, loss, logits], feed_dict={x_acoustic: x, y_timetofailure: y})
+        print('Pred: ' + str(np.array(out)[2]))
+        print('Actual: ' + str(y))
+        print('Loss: ' + str(np.array(out)[1]))
+        print('______')
